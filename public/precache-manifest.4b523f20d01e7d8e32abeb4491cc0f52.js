ffc4fc5ca9ddc06babb29f4592dd60aa4f1dd615
Fred011 <54903136+Fred011@users.noreply.github.com>
2019-12-20 09:18:17 +0100

    build

---
+++ public/precache-manifest.4b523f20d01e7d8e32abeb4491cc0f52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b37f631b8df0120a0d684edef73f5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56de6a206d75468da7b1",
+    "url": "/static/css/main.fc47eb6a.chunk.css"
+  },
+  {
+    "revision": "60b1a2bf61a09680edea",
+    "url": "/static/js/2.58dff7f2.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.58dff7f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56de6a206d75468da7b1",
+    "url": "/static/js/main.a4e6fae5.chunk.js"
+  },
+  {
+    "revision": "5bba74af4a4e52b458f9",
+    "url": "/static/js/runtime-main.e8ac4ea3.js"
+  }
+]);
